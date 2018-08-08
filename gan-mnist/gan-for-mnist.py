3d55f792dd9d1d516269790831382e70c415cf04
Chinh La <ducchinh.la@gmail.com>
2018-08-08 15:21:28 +0700

    a

--- gan-mnist/gan-for-mnist.py
+++ gan-mnist/gan-for-mnist.py
@@ -1,3 +1,11 @@
 import numpy as np 
-from PIL import image
-import cv2
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+from sklearn.utils import shuffle
+
+# Read data
+print('#LOAD DATA')
+mnist = input_data.read_data_sets('MNIST_data',one_hot = False)
+temp = mnist.train
+images, labels = temp.images, temp.labels
+images, labels = shuffle(np.asarray(images), np.asarray(labels))
