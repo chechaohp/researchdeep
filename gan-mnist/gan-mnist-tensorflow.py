aad6f6d954e6386885dcce34f1e0a6bb6c4e2ed5
chechaohp <chinh_a1_handsome@yahoo.com>
2018-08-16 11:46:02 +0700

    first final

--- gan-mnist/gan-mnist-tensorflow.py
+++ gan-mnist/gan-mnist-tensorflow.py
@@ -10,7 +10,6 @@ def plot(samples):
     fig = plt.figure(figsize=(4, 4))
     gs = gridspec.GridSpec(4, 4)
     gs.update(wspace=0.05, hspace=0.05)
-
     for i, sample in enumerate(samples):
         ax = plt.subplot(gs[i])
         plt.axis('off')
@@ -18,8 +17,9 @@ def plot(samples):
         ax.set_yticklabels([])
         ax.set_aspect('equal')
         plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
-
     return fig
+
+
 np.random.seed(1000)
 
 # Load data
@@ -28,7 +28,7 @@ temp = mnist.train
 images, labels = temp.images, temp.labels
 images, labels = shuffle(np.asarray(images), np.asarray(labels))
 epoch_num = 100
-learning_rate = 0.001
+# learning_rate = 0.001
 
 # Hyper parameters
 
@@ -41,25 +41,25 @@ hidden_input4, hidden_input5, hidden_input6 = 480, 560, 686
 X = tf.placeholder(dtype = tf.float32, name = 'X', shape = [1,images.shape[1]])
 # Y = tf.placeholder(dtype = tf.float32, name = 'Y', shape = labels.shape)
 # parameters to Discriminator Net
-D_W1 = tf.Variable(tf.zeros(shape = [images.shape[1],hidden_input1], dtype = tf.float32),name = 'D_W1')
+D_W1 = tf.Variable(tf.random_normal(shape = [images.shape[1],hidden_input1], dtype = tf.float32)*0.001,name = 'D_W1')
 D_b1 = tf.Variable(0.0, dtype = tf.float32, name = 'D_b1')
-D_W2 = tf.Variable(tf.zeros(shape = [hidden_input1,1], dtype = tf.float32), name = 'D_W2')
+D_W2 = tf.Variable(tf.random_normal(shape = [hidden_input1,1], dtype = tf.float32)*0.001, name = 'D_W2')
 D_b2 = tf.Variable(0.0, dtype = tf.float32, name = 'D_b2')
 
 # parameters for Generator Net
-G_W1 = tf.Variable(tf.zeros(shape = (G_input, hidden_input1),dtype = tf.float32),name = 'G_W1')
+G_W1 = tf.Variable(tf.random_normal(shape = (G_input, hidden_input1),dtype = tf.float32)*0.001,name = 'G_W1')
 G_b1 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b1') 
-G_W2 = tf.Variable(tf.zeros(shape = (hidden_input1, hidden_input2), dtype = tf.float32), name = 'G_W2')
+G_W2 = tf.Variable(tf.random_normal(shape = (hidden_input1, hidden_input2), dtype = tf.float32)*0.001, name = 'G_W2')
 G_b2 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b2')
-G_W3 = tf.Variable(tf.zeros(shape = (hidden_input2, hidden_input3), dtype = tf.float32), name = 'G_W3')
+G_W3 = tf.Variable(tf.random_normal(shape = (hidden_input2, hidden_input3), dtype = tf.float32)*0.001, name = 'G_W3')
 G_b3 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b3')
-G_W4 = tf.Variable(tf.zeros(shape = (hidden_input3, hidden_input4), dtype = tf.float32), name = 'G_W4')
+G_W4 = tf.Variable(tf.random_normal(shape = (hidden_input3, hidden_input4), dtype = tf.float32)*0.001, name = 'G_W4')
 G_b4 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b4')
-G_W5 = tf.Variable(tf.zeros(shape = (hidden_input4, hidden_input5), dtype = tf.float32), name = 'G_W5')
+G_W5 = tf.Variable(tf.random_normal(shape = (hidden_input4, hidden_input5), dtype = tf.float32)*0.001, name = 'G_W5')
 G_b5 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b5')
-G_W6 = tf.Variable(tf.zeros(shape = (hidden_input5, hidden_input6), dtype = tf.float32), name = 'G_W6')
+G_W6 = tf.Variable(tf.random_normal(shape = (hidden_input5, hidden_input6), dtype = tf.float32)*0.001, name = 'G_W6')
 G_b6 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b6')
-G_W7 = tf.Variable(tf.zeros(shape = (hidden_input6, 784), dtype = tf.float32), name = 'G_W7')
+G_W7 = tf.Variable(tf.random_normal(shape = (hidden_input6, 784), dtype = tf.float32)*0.001, name = 'G_W7')
 G_b7 = tf.Variable(0.0, dtype = tf.float32, name = 'G_b7')
 
 # Discriminator
@@ -67,7 +67,7 @@ def discriminator(x):
     D11 = tf.matmul(x, D_W1) + D_b1
     D11A = tf.nn.relu(D11)
     D12 = tf.matmul(D11A,D_W2) + D_b2
-    D12A = tf.log(D12)
+    D12A = tf.sigmoid(D12)
     return D12A
 
 # Generator
@@ -78,13 +78,13 @@ def generator(z):
     G12A = tf.nn.relu(G12)
     G13 = tf.matmul(G12A, G_W3) + G_b3
     G13A = tf.atan(G13)
-    G14 = tf.matmul(G13A, G_W4) + G_b3
+    G14 = tf.matmul(G13A, G_W4) + G_b4
     G14A = tf.nn.relu(G14)
-    G15 = tf.matmul(G14A, G_W5) + G_b3
+    G15 = tf.matmul(G14A, G_W5) + G_b5
     G15A = tf.tanh(G15)
-    G16 = tf.matmul(G15A, G_W6) + G_b3
+    G16 = tf.matmul(G15A, G_W6) + G_b6
     G16A = tf.nn.relu(G16)
-    G17 = tf.matmul(G16A, G_W7) + G_b3
+    G17 = tf.matmul(G16A, G_W7) + G_b7
     G17A = tf.sigmoid(G17)
     return G17A
 
@@ -96,32 +96,34 @@ G_sample = generator(Z)
 D_fake = discriminator(G_sample)
 D_real = discriminator(X)
 
-D_loss = -tf.log(D_real) + tf.log(1.0-D_fake)
-G_loss = -tf.log(D_fake)
+D_loss = tf.log(D_real) + tf.log(1.0-D_fake)
+G_loss = tf.log(D_fake)
 
-D_cost = tf.reduce_mean(D_loss)
-G_cost = tf.reduce_mean(G_loss)
+D_cost = -tf.reduce_mean(D_loss)
+G_cost = -tf.reduce_mean(G_loss)
 
-D_optimizer = tf.train.AdamOptimizer(learning_rate= 0.00009, beta1 = 0.9, beta2 = 0.999, epsilon= 0.00000001).minimize(D_cost)
-G_optimizer = tf.train.AdamOptimizer(learning_rate= 0.00009, beta1 = 0.9, beta2 = 0.999, epsilon= 0.00000001).minimize(G_cost)
+D_optimizer = tf.train.AdamOptimizer(learning_rate= 0.0001, beta1 = 0.9, beta2 = 0.999, epsilon= 0.00000001).minimize(D_cost, var_list = [D_W1,D_b1,D_W2,D_b2])
+G_optimizer = tf.train.AdamOptimizer(learning_rate= 0.0001, beta1 = 0.9, beta2 = 0.999, epsilon= 0.00000001).minimize(G_cost, var_list = [G_W1,G_b1,G_W2,G_b2,G_W3,G_b3,G_W4,G_b4,G_W5,G_b5,G_W6,G_b6,G_W7,G_b7])
 #
 init = tf.global_variables_initializer()
+
 loss = []
 D = []
 G = []
 with tf.Session() as sess:
     sess.run(init)
-    for epoch in range(epoch_num):
+    for epoch in range(0,epoch_num+1):
         random_int = np.random.randint(len(images)-5)
         current_image = np.expand_dims(images[random_int],axis = 0)
         sample_Z = sess.run(tf.random_uniform(minval = -1.,maxval = 1.,shape = [1,G_input]))
         D = sess.run([D_optimizer,D_cost],feed_dict = {X: current_image, Z:sample_Z})
         G = sess.run(G_optimizer,feed_dict = {Z:sample_Z})
         # print(sess.run(D_cost))
-        loss += [sess.run(D_cost, feed_dict = {X: current_image, Z:sample_Z})]
-        # if epoch % 10 == 0:
+        # loss += [sess.run(D_cost, feed_dict = {X: current_image, Z:sample_Z})]
+        if epoch % 10 == 0:
             print(sess.run(D_cost,feed_dict = {X: current_image, Z:sample_Z}))
             print(sess.run(G_cost,feed_dict = {Z:sample_Z}))
-            # fig = plot(sess.run(generator(sample_Z)))
-            # fig.savefig('Click_Me_{}.png', bbox_inches='tight')
-        
+            fig = plot(sess.run(generator(Z),feed_dict = {Z:sample_Z}))
+            fig.savefig('Click_Me_{}.png'.format(epoch), bbox_inches='tight')
+            # print(sess.run(generator(Z),feed_dict = {Z:sample_Z}))
+# tf.train.AdamOptimizer()
